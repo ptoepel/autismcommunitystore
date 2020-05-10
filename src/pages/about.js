beb6b3f9fee47b5d6d49d6fae2fdcae2436351bf
Patrick Toepel <patrick.toepel@gmail.com>
2020-05-10 11:26:32 -0600

    About Page Start

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,14 +8,13 @@ import Layout from "../components/layout"
 const AboutPage = ( {data} ) => (
   <Layout>
 
-    <h1>Hi people</h1>
+    <h1>About</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
 
 
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+
   </Layout>
 )
 
